deff2d78bbe468eed40ba8f5c82da45e1890cf6b
ishehroze <ishehroze@gmail.com>
2017-02-09 15:25:00 +0600

    Added blood group filter in student modeladmin, fixed overdue query

--- students/admin.py
+++ students/admin.py
@@ -37,16 +37,60 @@ class StatusFilter(admin.SimpleListFilter):
         today = date.today()
 
         if self.value() == 'overdue':
-            return queryset.filter(due_date__gt=today)\
+            return queryset.filter(due_date__lt=today)\
                 .exclude(amount_paid=F('amount_total'))\
                 .exclude(expiration_date__lt=today)
 
-        if self.value() == 'paid':
+        elif self.value() == 'paid':
             return queryset.filter(amount_paid=F('amount_total'))\
                 .exclude(expiration_date__lt=today)
 
-        if self.value() == 'expired':
+        elif self.value() == 'expired':
             return queryset.filter(expiration_date__lt=today)
+
+        else:
+            return queryset
+
+class BloodGroupFilter(admin.SimpleListFilter):
+    title = _('blood group')
+    parameter_name = 'bldgrp'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('opos', _('O+')),
+            ('apos', _('A+')),
+            ('bpos', _('B+')),
+            ('abpos', _('AB+')),
+            ('oneg', _('O-')),
+            ('aneg', _('A-')),
+            ('bneg', _('B-')),
+            ('abneg', _('AB-')),
+            ('unset', _('Unassigned'))
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'opos':
+            return queryset.filter(blood_group='o+')
+        elif self.value() == 'apos':
+            return queryset.filter(blood_group='a+')
+        elif self.value() == 'bpos':
+            return queryset.filter(blood_group='b+')
+        elif self.value() == 'abpos':
+            return queryset.filter(blood_group='ab+')
+        elif self.value() == 'oneg':
+            return queryset.filter(blood_group='o-')
+        elif self.value() == 'aneg':
+            return queryset.filter(blood_group='a-')
+        elif self.value() == 'bneg':
+            return queryset.filter(blood_group='b-')
+        elif self.value() == 'abneg':
+            return queryset.filter(blood_group='ab-')
+        elif self.value() == 'unset':
+            return queryset.exclude(blood_group__in=(
+                'o+', 'a+', 'b+', 'ab+', 'o-', 'a-', 'b-', 'ab-'
+            ))
+        else:
+            return queryset
 
 class StudentAdmin(admin.ModelAdmin):
     readonly_fields = ('payment_info',)
@@ -68,6 +112,7 @@ class StudentAdmin(admin.ModelAdmin):
         'batch',
         'is_prospective',
         'is_assistive',
+        BloodGroupFilter,
     )
 
     search_fields = ('roll', 'name',)
